c8019c18b5e5cc852baa85f2f29d22f2de4b8052
Miracle Rapheal <miraclerapheal14@gmail.com>
2025-10-19 13:43:42 +0100

    feat(repository): add StudentRepository interface extending JpaRepository

---
+++ src/main/java/com/rapheal/student_management_system/repositories/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.rapheal.student_management_system.repositories;
+
+import com.rapheal.student_management_system.Entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+}
